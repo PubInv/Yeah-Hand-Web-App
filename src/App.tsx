904ca40d81a43c829e1ddeaa3ad3dd38b0be6594
GoodKimchi <224943594+GoodKimchi@users.noreply.github.com>
2026-06-20 02:58:36 -0700

    Fixed Warning

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,12 @@ function App() {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify({
-                    command,
+                    command: commandToSend,
                 }),
             });
 
             const data = await response.json();
-            const commandLabel = command.split(" ")[0];
+            const commandLabel = commandToSend.split(" ")[0];
 
             setStatus(
                 data.success
